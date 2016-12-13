132042c5472af9ec980385b5534461e5f146a57c
sam <sam@davton.com>
2016-12-13 19:02:23 +0530

    commit

---
+++ latestwork/PartyInvites/PartyInvitesPro/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using PartyInvitesPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvitesPro.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: 
+        [HttpGet]
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RSVPFRM()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult RSVPFRM(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+
+                // TODO: Email response to the party organizer
+
+                return View("Thanks", guestResponse);
+
+            }
+            else
+            {
+
+                // there is a validation error
+
+                return View();
+
+            }
+           // return View("Thanks",guestResponse);
+        }
+    }
+}
---
+++ latestwork/PartyInvites/PartyInvitesPro/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyInvitesPro.MvcApplication" Language="C#" %>
