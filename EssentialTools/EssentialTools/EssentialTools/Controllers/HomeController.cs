29380b77974175fa3f6ad3077b15ab030c99c4b7
sam <sam@davton.com>
2016-12-13 22:05:40 +0530

    commit

--- EssentialTools/EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EssentialTools.Models;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,32 @@ namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+
+        }
+
+        private Product[] products = {new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+
+new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}};
+
         public ActionResult Index()
         {
-            return View();
+           // IKernel ninjectKernel = new StandardKernel();
+           // ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+           // ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            //IValueCalculator calc = ninjectKernel.Get<IValueCalculator>(); //new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
